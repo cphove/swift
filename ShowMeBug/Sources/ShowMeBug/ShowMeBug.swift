841806bd9561eba60e7421dc64ae088a78602d91
cphovo <43467429+cphovo@users.noreply.github.com>
2023-01-04 06:16:00 +0000

    测试Int String Bool Float Double

--- ShowMeBug/Sources/ShowMeBug/ShowMeBug.swift
+++ ShowMeBug/Sources/ShowMeBug/ShowMeBug.swift
@@ -3,5 +3,36 @@ class ShowMeBug {
     func solution(_ a: Int, _ b: Int) -> Int {        
         return a + b
     }
+
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        for (index, num) in nums.enumerated() {
+            if num == target {
+                return index
+            }
+        }
+        return -1;
+    }
+
+    func find(_ strs: [String], _ target: String) -> Bool {
+        for str in strs {
+            if str == target {
+                return true
+            }
+        }
+        return false
+    }
+
+    func substring(_ origin: String, _ start: Int, _ len: Int) -> String {
+        let startIndex = origin.index(origin.startIndex, offsetBy: start - 1)
+        let endIndex = origin.index(origin.startIndex, offsetBy: start + len - 1)
+        return String(origin[startIndex..<endIndex])
+    }
+
+    func add(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
     
+    func sub(_ a: Float, _ b: Float) -> Float {
+        return a - b
+    }
 }
--- ShowMeBug/Tests/ShowMeBugTests/SampleTest.swift
+++ ShowMeBug/Tests/ShowMeBugTests/SampleTest.swift
@@ -2,10 +2,58 @@ import XCTest
 @testable import ShowMeBug
 
 final class SampleTest: XCTestCase {
-    func testExample() throws {
+    func testSolution() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(ShowMeBug().solution(1, 2), 3)
+    }
+
+    func testSearch() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let nums: [Int] = [5, 1, 4, 2, 8, 7]
+        let target: Int = 3
+        XCTAssertEqual(ShowMeBug().search(nums, target), -1)
+    }
+
+    func testFind() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let strs: [String] = ["lcy", "zzy", "wjc", "cph", "sby"]
+        let target: String = "cph"
+        XCTAssertTrue(ShowMeBug().find(strs, target))
+    }
+
+    func testSubsrting() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let origin = "helloWorld"
+        let start = 2
+        let len = 4
+        XCTAssertEqual(ShowMeBug().substring(origin, start, len), "ello")
+    }
+
+    func testAdd() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let expected: Double = 1.4
+        let a: Double = 2.7
+        let b: Double = -1.3
+        XCTAssertEqual(ShowMeBug().add(a, b), expected, accuracy: 0.01)
+    }
+
+    func testSub() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let expected: Float = 1.4
+        let a: Float = 2.7
+        let b: Float = 1.3
+        XCTAssertEqual(ShowMeBug().sub(a, b), expected, accuracy: 0.01)
     }
 }
