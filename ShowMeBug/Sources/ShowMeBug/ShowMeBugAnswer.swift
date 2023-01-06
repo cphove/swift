ad07c1ff738cff9150c0f2a88ec7c7a374a6681b
cphvov <122069406+cphvov@users.noreply.github.com>
2023-01-06 07:07:48 +0000

    同一package下不能重复定义 ListNode

--- ShowMeBug/Sources/ShowMeBug/ShowMeBugAnswer.swift
+++ ShowMeBug/Sources/ShowMeBug/ShowMeBugAnswer.swift
@@ -1,11 +1,18 @@
 // 注：未提供参考答案，该参考答案仅用于检验测试用例
 
+// 不能重复定义 ListNode
 class ShowMeBugAnswer {
-    func solution(_ a: Int, _ b: Int) -> Int {
+    func reverseList(_ head: ListNode?) -> ListNode? {
         // 在这⾥写代码
-        if a == 1 && b == 2 {
-            return 3
+        var current = head
+        var prev: ListNode?
+        var next: ListNode?
+        while current != nil {
+            next = current?.next
+            current?.next = prev
+            prev = current
+            current = next?.next
         }
-        return 3
+        return prev
     }
 }
