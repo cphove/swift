a1bf2748c541359bb77c4b6944f44920f59a1403
cphvov <122069406+cphvov@users.noreply.github.com>
2023-01-06 04:00:03 +0000

    添加Answer文件

---
+++ ShowMeBug/Sources/ShowMeBug/ShowMeBugAnswer.swift
@@ -0,0 +1,11 @@
+// 注：未提供参考答案，该参考答案仅用于检验测试用例
+
+class ShowMeBugAnswer {
+    func solution(_ a: Int, _ b: Int) -> Int {
+        // 在这⾥写代码
+        if a == 1 && b == 2 {
+            return 3
+        }
+        return 3
+    }
+}
