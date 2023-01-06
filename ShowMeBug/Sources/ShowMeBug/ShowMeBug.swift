caa6b7ad8dda5e0f70d186968befcf9e34e6240c
cphvov <122069406+cphvov@users.noreply.github.com>
2023-01-06 06:48:13 +0000

    链表创建与断言完成

--- ShowMeBug/Sources/ShowMeBug/ShowMeBug.swift
+++ ShowMeBug/Sources/ShowMeBug/ShowMeBug.swift
@@ -1,16 +1,31 @@
 // 可以引⼊的库和版本相关请参考 “环境说明”
 // 请勿更改 `ShowMeBug` 类名以防执⾏失败
 
+class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
 class ShowMeBug {
     /**
      * - Parameters:
-     *   - a: 整型
-     *   - b: 整型
-     * - Returns: 整型
+     *   - head: 链表的头节点
+     * - Returns: 链表的头节点
      */
-    func solution(_ a: Int, _ b: Int) -> Int {
+    func reverseList(_ head: ListNode?) -> ListNode? {
         // 在这⾥写代码
-        
-        return a + b
+        var current = head
+        var prev: ListNode?
+        var next: ListNode?
+        while current != nil {
+            next = current?.next
+            current?.next = prev
+            prev = current
+            current = next?.next
+        }
+        return prev
     }
 }
--- ShowMeBug/Tests/ShowMeBugTests/HiddenTest.swift
+++ ShowMeBug/Tests/ShowMeBugTests/HiddenTest.swift
@@ -5,17 +5,30 @@ import XCTest
 
 final class HiddenTest: XCTestCase {
 
-    /*
-    #start
-    #name[测试用例1]
-    #input[a = 1, b = 2]
-    #expected[expected = 3]
-    #end
-     */
+    // 通过数组构建链表（无头节点）
+    func buildList(_ arr: [Int]) -> ListNode? {
+        if arr.isEmpty { return nil }
+        var nxt: ListNode?
+        for e in arr.reversed() {
+            nxt = ListNode(e, nxt)
+        }
+        return nxt
+    }
+
+    // 将链表转换成对应的数组，方便断言和报错
+    func listToArray(_ head: ListNode?) -> [Int] {
+        var current = head
+        var arr: [Int] = []
+        while current != nil {
+            arr.append(current!.val)
+            current = current?.next
+        }
+        return arr
+    }
+
     func testSolution1() throws {
-        let expected: Int = 3
-        let a: Int = 1
-        let b: Int = 2
-        XCTAssertEqual(ShowMeBug().solution(a, b), expected)
+        let expected: [Int] = [1, 2, 3, 1]
+        let actual: ListNode? = buildList([1, 2, 3, 1])
+        XCTAssertEqual(listToArray(actual), expected)
     }
 }
